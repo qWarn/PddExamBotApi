a60c1548469957aaff1d98bc9cdb98aef1c83191
qWarn <kirilov-nikitka@list.ru>
2024-02-17 02:58:52 +0300

    Renamed from SelectedQuestionsRepository

--- src/main/java/ru/qwarn/pddexambotapi/repositories/SelectedRepository.java
+++ src/main/java/ru/qwarn/pddexambotapi/repositories/SelectedRepository.java
@@ -3,7 +3,8 @@ package ru.qwarn.pddexambotapi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.qwarn.pddexambotapi.models.Question;
-import ru.qwarn.pddexambotapi.models.SelectedQuestions;
+import ru.qwarn.pddexambotapi.models.Selected;
+import ru.qwarn.pddexambotapi.models.SelectedId;
 import ru.qwarn.pddexambotapi.models.User;
 
 
@@ -11,14 +12,14 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface SelectedQuestionsRepository extends JpaRepository<SelectedQuestions, Integer> {
+public interface SelectedRepository extends JpaRepository<Selected, SelectedId> {
 
-    Optional<SelectedQuestions> findByQuestionAndUser(Question question, User user);
+    Optional<Selected> findByQuestionAndUser(Question question, User user);
 
     void removeByQuestionAndUser(Question question, User user);
 
-    List<SelectedQuestions> findAllByUserAndAlreadyWasFalse(User user);
+    List<Selected> findAllByUserAndAlreadyWasFalse(User user);
 
-    List<SelectedQuestions> findAllByUser(User user);
+    List<Selected> findAllByUser(User user);
 
 }
