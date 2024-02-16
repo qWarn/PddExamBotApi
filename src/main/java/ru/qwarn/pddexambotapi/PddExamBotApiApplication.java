16adcc9922c741b1ea5cbf2ee604b6533e720815
qWarn <kirilov-nikitka@list.ru>
2024-02-17 02:57:52 +0300

    Removed unused imports

--- src/main/java/ru/qwarn/pddexambotapi/PddExamBotApiApplication.java
+++ src/main/java/ru/qwarn/pddexambotapi/PddExamBotApiApplication.java
@@ -1,20 +1,14 @@
 package ru.qwarn.pddexambotapi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.qwarn.pddexambotapi.controllers.TelegramBotController;
-
-import java.util.Random;
 
 @SpringBootApplication
 public class PddExamBotApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PddExamBotApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PddExamBotApiApplication.class, args);
+    }
 
 
 }
