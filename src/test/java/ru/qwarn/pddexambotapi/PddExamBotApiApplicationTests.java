23d0990225398c9a9bfda75c779668ab00a32d04
qWarn <kirilov-nikitka@list.ru>
2024-02-17 03:18:47 +0300

    Added contextLoads test

--- src/test/java/ru/qwarn/pddexambotapi/PddExamBotApiApplicationTests.java
+++ src/test/java/ru/qwarn/pddexambotapi/PddExamBotApiApplicationTests.java
@@ -1,13 +1,20 @@
 package ru.qwarn.pddexambotapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.qwarn.pddexambotapi.controllers.TelegramBotController;
+
+import static org.springframework.util.Assert.*;
 
 @SpringBootTest
 class PddExamBotApiApplicationTests {
+	@Autowired
+	private TelegramBotController telegramBotController;
 
 	@Test
 	void contextLoads() {
+		notNull(telegramBotController, "Context loads failed: didn't load TelegramBotController");
 	}
 
 }
