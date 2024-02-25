b363ef41a6399acae6a94cb3e66536ae70b614aa
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:29:08 +0300

    Changed HttpStatus

--- src/main/java/ru/qwarn/pddexambotapi/exceptions/SelectedIsEmptyException.java
+++ src/main/java/ru/qwarn/pddexambotapi/exceptions/SelectedIsEmptyException.java
@@ -3,8 +3,8 @@ package ru.qwarn.pddexambotapi.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class SelectedIsEmptyException extends TelegramBotCustomException{
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class SelectedIsEmptyException extends TelegramBotCustomException {
     public SelectedIsEmptyException(String message) {
         super(message);
     }
