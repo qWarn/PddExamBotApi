28cef8afd7a7db8b52d976436f53210f80258a0d
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:26:25 +0300

    Updated AnswerRepository

--- src/main/java/ru/qwarn/pddexambotapi/repositories/AnswerRepository.java
+++ src/main/java/ru/qwarn/pddexambotapi/repositories/AnswerRepository.java
@@ -3,11 +3,13 @@ package ru.qwarn.pddexambotapi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.qwarn.pddexambotapi.models.Answer;
+import ru.qwarn.pddexambotapi.models.Question;
 
 
 import java.util.List;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-    List<Answer> getByQuestionId(int questionId);
+    List<Answer> findAllByQuestion(Question question);
+
 }
