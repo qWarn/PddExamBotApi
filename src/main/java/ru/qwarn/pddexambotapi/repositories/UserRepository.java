81594e8265655958b7b9553f30d4a84af628ac69
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:36:28 +0300

    Updated UserRepository

--- src/main/java/ru/qwarn/pddexambotapi/repositories/UserRepository.java
+++ src/main/java/ru/qwarn/pddexambotapi/repositories/UserRepository.java
@@ -4,11 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.qwarn.pddexambotapi.models.User;
 
-import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    Optional<User> findByChatId(long chatId);
-
-}
+public interface UserRepository extends JpaRepository<User, Long> {}
