4eb8fd04bd67e77d794b3cc9127efbcba23e42db
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:35:23 +0300

    Updated TelegramBotService

--- src/main/java/ru/qwarn/pddexambotapi/services/TelegramBotService.java
+++ src/main/java/ru/qwarn/pddexambotapi/services/TelegramBotService.java
@@ -8,17 +8,15 @@ import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.qwarn.pddexambotapi.dto.TaskDTO;
-import ru.qwarn.pddexambotapi.exceptions.InvalidAnswerException;
-import ru.qwarn.pddexambotapi.exceptions.QuestionNotFoundException;
-import ru.qwarn.pddexambotapi.models.Answer;
-import ru.qwarn.pddexambotapi.models.Question;
-import ru.qwarn.pddexambotapi.models.User;
+import ru.qwarn.pddexambotapi.models.*;
 import ru.qwarn.pddexambotapi.utils.MessageCreator;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+
+import static ru.qwarn.pddexambotapi.models.TaskType.*;
 
 @Service
 @RequiredArgsConstructor
@@ -31,85 +29,91 @@ public class TelegramBotService {
     private final AnswerService answerService;
 
     @Transactional
-    public ResponseEntity<PartialBotApiMethod<Message>> generateQuestion(long chatId, TaskDTO taskDTO) {
+    public ResponseEntity<PartialBotApiMethod<Message>> getQuestionResponse(long chatId) {
         User user = userService.getByChatId(chatId);
-        defineTaskType(taskDTO, user);
-
         Question currentQuestion = user.getQuestion();
-
-        if (user.getTaskType().equals("ticket")) {
-            if (currentQuestion.getOrderInTicket() == 20) {
-                return userService.endMessage(chatId);
+        if (user.getTaskType().equals(TICKET)) {
+            if (currentQuestion == null) {
+                return userService.getFinishMessage(chatId);
             }
-            return questionResponse(chatId, currentQuestion);
+            return initQuestion(chatId, currentQuestion);
         } else {
-            Optional<Question> questionOptional = selectedService.generateQuestionFromSelected(user);
+            Optional<Question> questionOptional = selectedService.getQuestionFromSelected(user);
             if (questionOptional.isEmpty()) {
-                return userService.endMessage(chatId);
+                return userService.getFinishMessage(chatId);
             }
             user.setQuestion(questionOptional.get());
 
-            return questionResponse(chatId, questionOptional.get());
+            return initQuestion(chatId, questionOptional.get());
         }
     }
 
-    private void defineTaskType(TaskDTO taskDTO, User user) {
-        if (taskDTO != null && taskDTO.type().equals("selected")) {
-            user.setFailsCount(0);
-            user.setTaskType(taskDTO.type());
-            selectedService.refreshSelectedQuestions(user);
-        } else if (taskDTO != null && taskDTO.type().equals("ticket")) {
-            user.setFailsCount(0);
-            user.setTaskType(taskDTO.type());
+    @Transactional
+    public ResponseEntity<SendMessage> getStartTaskResponse(long chatId, TaskDTO taskDTO) {
+        User user = userService.getByChatId(chatId);
+        user.setFailsCount(0);
+        user.setTaskType(taskDTO.taskType());
+        if (taskDTO.taskType().equals(TICKET)) {
             user.setQuestion(questionService.getFirstQuestion(taskDTO.ticketId()));
         }
+        if (taskDTO.taskType().equals(SELECTED)) {
+            selectedService.refreshSelectedQuestions(user);
+        }
+
+        return ResponseEntity.ok(MessageCreator.createStartMessage(chatId, taskDTO));
+    }
+
+    public ResponseEntity<SendMessage> getCorrectAnswerResponse(long chatId){
+        User user = userService.getByChatId(chatId);
+        Question question = user.getQuestion();
+        return ResponseEntity.ok(MessageCreator.createAnswer(chatId, question));
     }
 
 
-    private ResponseEntity<PartialBotApiMethod<Message>> questionResponse(long chatId, Question question) {
-        List<Answer> answers = answerService.getAnswers(question.getId());
-
+    private ResponseEntity<PartialBotApiMethod<Message>> initQuestion(long chatId, Question question) {
+        List<Answer> answers = answerService.getAnswers(question);
+        boolean isSelected = selectedService.getById(chatId, question.getId()).isPresent();
         if (question.getImageURI() == null) {
             return ResponseEntity.ok().header("MessageType", "sendmessage")
-                    .body(MessageCreator.createQuestionWithoutMessage(chatId, question, answers));
+                    .body(MessageCreator.createQuestionWithoutMessage(chatId, question, answers, isSelected));
         } else {
             return ResponseEntity.ok().header("MessageType", "sendphoto")
-                    .body(MessageCreator.createQuestionWithImage(chatId, question, answers));
+                    .body(MessageCreator.createQuestionWithImage(chatId, question, answers, isSelected));
         }
     }
 
     @Transactional
-    public ResponseEntity<SendMessage> generateGreetingsOrTickets(long chatId) {
-        return userService.sendGreetingOrTickets(chatId);
+    public ResponseEntity<SendMessage> getGreetingMessageResponse(long chatId) {
+        return userService.getGreetingMessage(chatId);
     }
 
     @Transactional
-    public ResponseEntity<SendMessage> generateTickets(long chatId, boolean next) {
+    public ResponseEntity<SendMessage> getTicketsResponse(long chatId, boolean next) {
         User user = userService.getByChatId(chatId);
-        user.setTaskType("none");
+        user.setTaskType(NONE);
         user.setQuestion(null);
         user.setFailsCount(0);
 
-        return ResponseEntity.ok(MessageCreator.createTicketsMessage(user, next));
+        return ResponseEntity.ok(MessageCreator.createTicketsMessage(chatId, next));
     }
 
     @Transactional
-    public ResponseEntity<SendMessage> generateAnswerResult(long chatId, int answerNumber) {
+    public void checkAnswer(long chatId, int answerNumber) {
         User user = userService.getByChatId(chatId);
         Question currQuestion = user.getQuestion();
-        if (currQuestion == null) {
-            throw new QuestionNotFoundException("Вы не выбрали билет!");
-        }
-        if (answerNumber > answerService.getAnswers(currQuestion.getId()).size() || answerNumber <= 0) {
-            throw new InvalidAnswerException("Ответ под номером " + answerNumber + " не существует!");
-        }
 
+        if (user.getTaskType().equals(SELECTED)){
+            selectedService.getById(chatId, currQuestion.getId()).ifPresent(question -> question.setAlreadyWas(true));
+        }else {
+            if (currQuestion.getOrderInTicket() == 20){
+                user.setQuestion(null);
+            }else {
+                user.setQuestion(questionService.getById(currQuestion.getId() + 1));
+            }
+        }
         boolean isCorrect = currQuestion.getCorrectAnswerNumber() == answerNumber;
         user.setFailsCount(user.getFailsCount() + (isCorrect ? 0 : 1));
-        user.setQuestion(questionService.getById(currQuestion.getId() + 1));
         user.setLastActive(Timestamp.from(Instant.now()));
-        return ResponseEntity.ok(MessageCreator
-                .createAnswer(chatId, currQuestion, selectedService.getByQuestionAndUser(currQuestion, user), isCorrect));
     }
 
     @Transactional
@@ -119,7 +123,7 @@ public class TelegramBotService {
 
     @Transactional
     public void removeQuestionFromSelected(long chatId, int questionId) {
-        selectedService.removeFromSelected(questionService.getById(questionId), userService.getByChatId(chatId));
+        selectedService.removeFromSelected(userService.getByChatId(chatId), questionService.getById(questionId));
     }
 
 }
