46634990b1694f76b7c875e16f79fb33b7f14521
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:28:37 +0300

    Updated SelectedId

--- src/main/java/ru/qwarn/pddexambotapi/models/SelectedId.java
+++ src/main/java/ru/qwarn/pddexambotapi/models/SelectedId.java
@@ -1,12 +1,16 @@
 package ru.qwarn.pddexambotapi.models;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SelectedId implements Serializable {
     long userId;
     int questionId;
