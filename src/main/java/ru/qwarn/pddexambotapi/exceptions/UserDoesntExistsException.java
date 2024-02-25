1a2de307a01cce7c73c3ee6ea29deabf6cd68c03
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:36:11 +0300

    Updated UserDoesntExistsException

--- src/main/java/ru/qwarn/pddexambotapi/exceptions/UserDoesntExistsException.java
+++ src/main/java/ru/qwarn/pddexambotapi/exceptions/UserDoesntExistsException.java
@@ -4,8 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UserDoesntExistsException extends TelegramBotCustomException{
-
+public class UserDoesntExistsException extends TelegramBotCustomException {
     public UserDoesntExistsException() {
         super("Пожалуйста, введите команду /start");
     }
