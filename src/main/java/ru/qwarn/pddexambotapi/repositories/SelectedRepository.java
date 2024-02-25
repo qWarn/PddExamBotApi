0c998a9e3f3c3c78b97f10e793bb68d50a56c6cd
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:29:30 +0300

    Updated SelectedRepository

--- src/main/java/ru/qwarn/pddexambotapi/repositories/SelectedRepository.java
+++ src/main/java/ru/qwarn/pddexambotapi/repositories/SelectedRepository.java
@@ -2,21 +2,15 @@ package ru.qwarn.pddexambotapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.qwarn.pddexambotapi.models.Question;
 import ru.qwarn.pddexambotapi.models.Selected;
 import ru.qwarn.pddexambotapi.models.SelectedId;
 import ru.qwarn.pddexambotapi.models.User;
 
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SelectedRepository extends JpaRepository<Selected, SelectedId> {
-
-    Optional<Selected> findByQuestionAndUser(Question question, User user);
-
-    void removeByQuestionAndUser(Question question, User user);
 
     List<Selected> findAllByUserAndAlreadyWasFalse(User user);
 
